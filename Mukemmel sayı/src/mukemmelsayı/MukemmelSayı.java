edf760906fb4a59eb6b7d4c20be7e4c66238d05a
FeritKIRAY <110461580+FeritKIRAY@users.noreply.github.com>
2022-08-21 22:07:35 +0300

    Add files via upload

---
+++ Mukemmel sayı/src/mukemmelsayı/MukemmelSayı.java
@@ -0,0 +1,25 @@
+package mukemmelsayı;
+import java.util.Scanner;
+public class MukemmelSayı {
+
+	public static void main(String[] args) {
+		
+		 int a,n=0;
+	        int i;
+	        Scanner inp=new Scanner(System.in);
+	        System.out.println("Lutfen bir sayi giriniz:");
+	        a= inp.nextInt();
+	        for (i=1; i<a; i++) {
+	            if (a % i == 0)
+	                n=i+n;
+	        }
+	        if (n==a)
+	            System.out.println("sectiginiz sayi mukemmel sayidir");
+	        else
+	            System.out.println("sectiginiz sayi mukemmel sayi degildir.");
+	            System.out.println("Tam bolunen sayilarin toplami="+n);
+		
+		
+	}
+
+}
